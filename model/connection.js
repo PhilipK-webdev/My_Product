3093b9b22999066c62c78fde2b69edbd46180c14
PhilipK-webdev <philipk179@gmail.com>
2021-10-24 13:56:09 +0300

    ADD:create server and connect to mongoDB

---
+++ model/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const MONGODB_URI = process.env.MONGODB_URL || "mongodb://localhost/products";
+mongoose
+    .connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // useFindAndModify: false,
+    })
+    .then(() => console.log("you are connected to mongodb"))
+    .catch((err) => console.log(err));
+
+module.exports = mongoose;
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 5000;
+require("./model/connection");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const apiRoutes = require("./routes/api-routes");
+app.use(apiRoutes);
+
+app.listen(PORT, () => {
+    console.log(`listening at http://localhost:${PORT}`);
+});
